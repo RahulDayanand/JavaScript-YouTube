92264615240ca4389ddc1321a7b76c162d3560eb
RahulDayanand <101518605+RahulDayanand@users.noreply.github.com>
2024-03-12 07:13:11 +0000

    DataTypes

---
+++ 01_Basics/DataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // To treat all JS Code as newer version
+
+//alert(3 + 3) - this will show error for alert as this is node js and not a browser
+
+console.log(3+2) //code readability should be high
+console.log("Rahul")
+
+let name = "Rahul D"
+let age = 24
+let isLoggedIn = true
+let state = null
+
+//Primitive Data Types are listed below
+
+//Number -> Range - 2 to Power 53
+//BigInt
+//Boolean -> True/False
+//String -> " "
+//Undefined -> The varibale which is declared but no value assigned to it
+//null -> Standalone value
+//symbol -> unique(To identify which component(React) in unique)
+
+console.log(typeof(null))
+console.log(typeof "")
+console.log(typeof undefined)
