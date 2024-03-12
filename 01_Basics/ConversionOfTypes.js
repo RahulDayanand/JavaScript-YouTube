11be3db497ceeee58c219e183124112b86c10e18
RahulDayanand <101518605+RahulDayanand@users.noreply.github.com>
2024-03-12 08:45:57 +0000

    Data Type Conversion

---
+++ 01_Basics/ConversionOfTypes.js
@@ -0,0 +1,66 @@
+let n = 35;
+
+let c = String(n);
+console.log(c);
+console.log(typeof c);
+
+// let a = "Rahul";
+
+// let c = Boolean(a);
+// console.log(c);
+// console.log(typeof c);
+
+// 0 - > false, 1 -> true
+// "" -> false
+
+// let a = "";
+
+// let c = Boolean(a);
+// console.log(c);
+// console.log(typeof c);
+
+// let a = 1;
+
+// let c = Boolean(a);
+// console.log(c);
+// console.log(typeof c);
+
+// let a = "Rahul";
+// console.log(typeof a);
+
+// let c= Number(a);
+// console.log(typeof c);
+// console.log(typeof a);
+// console.log(a);
+// console.log(c);
+
+// "33" -> 33
+// "33abc" -> NaN(Type is Number)
+// true -> 1 , false -> 0
+
+// let a = undefined;
+// console.log(typeof a);
+
+// let c= Number(a);
+// console.log(typeof c);
+// console.log(typeof a);
+// console.log(a);
+// console.log(c);
+
+// let a = null;
+// console.log(typeof a);
+
+// let c= Number(a);
+// console.log(typeof c);
+// console.log(typeof a);
+// console.log(a);
+// console.log(c);
+
+// let a = "10abc";
+// console.log(typeof a);
+
+// let c= Number(a);
+// console.log(typeof c);
+// console.log(typeof a);
+// console.log(a);
+// console.log(c);
