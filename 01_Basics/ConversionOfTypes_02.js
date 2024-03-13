d654364e8b1fcff7ef71d2733cab8e3716851a1f
RahulDayanand <101518605+RahulDayanand@users.noreply.github.com>
2024-03-13 08:33:46 +0000

    Data Type Conversion 02

---
+++ 01_Basics/ConversionOfTypes_02.js
@@ -0,0 +1,18 @@
+let str1 = "Hello";
+let str2 = " World";
+let str3 = str1 + str2;
+
+console.log(str3);
+
+console.log(1 + "2");
+console.log("2" + 1);
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "1");
+
+console.log(+true);
+console.log(+"");
+
+let num1 = 10;
+
+console.log(num1++);
+console.log(++num1);
