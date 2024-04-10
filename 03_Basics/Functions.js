68a721f65c3254d8abcc9d23cc6d4e0ea07da997
RahulDayanand <101518605+RahulDayanand@users.noreply.github.com>
2024-04-10 07:31:59 +0000

    Funtions_Part1

--- 03_Basics/Functions.js
+++ 03_Basics/Functions.js
@@ -7,3 +7,39 @@ function basic(){
 }
 
 basic();
+
+function addition(number1,number2){
+    return number1 + number2;
+    console.log(number1 + number2); //anything declared under return statement will be ignored
+}
+
+const result = addition(2,5);
+console.log(result);
+
+// function LoginUserMessage(username){
+//     return `${username} just logged in`;
+// }
+
+// console.log(LoginUserMessage("Rahul"));
+
+// function LoginUserMessage(username){
+
+//     if(username === undefined){
+//        console.log("Please enter the username");
+//        return;
+//     }
+//     return `${username} just logged in`;
+// }
+
+// console.log(LoginUserMessage());
+
+function LoginUserMessage(username = "User"){
+
+    if(!username){
+       console.log("Please enter the username");
+       return;
+    }
+    return `${username} just logged in`;
+}
+
+console.log(LoginUserMessage("Rahul"));  //It will override the "username = "user" and will print Rahul"
