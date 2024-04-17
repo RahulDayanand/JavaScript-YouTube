fd797ecba62fa5f05110445894bbeb5c2a534a8b
RahulDayanand <101518605+RahulDayanand@users.noreply.github.com>
2024-04-17 08:58:50 +0000

    scope level & mini hoisting

--- 03_Basics/scope_level_mini_hoisting.js
+++ 03_Basics/scope_level_mini_hoisting.js
@@ -14,3 +14,29 @@ function a(){
 }
 
 a();
+
+if(true){
+    const username = "Harsha";
+    if(username === "Harsha"){
+        const website = " youtube";
+        console.log(username + website);
+    }
+    //console.log(website);   //Referene Error
+}
+
+//console.log(username);  //Reference Error
+
+console.log(addthree(3));
+function addthree(num){
+    return num + 3;
+}
+
+//console.log(addthree(3));
+
+console.log(addfive(5));     //Hoisting - ReferenceError: Cannot access 'addfive' before initialization
+
+const addfive = function(num){
+   return num + 5;
+}
+
+//console.log(addfive(5));
